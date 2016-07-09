1cb6f6c52fb30ab33c71e8da0152e7a8547520d1
Alex-MTX <alexander.molinari@gmail.com>
2016-07-09 19:33:29 -0300

    very initial commit. nothing interest yet.

---
+++ EventTraceExperiments/DNSProblem.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Parsers.MicrosoftWindowsDNSClient;
+using Microsoft.Diagnostics.Tracing.Session;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTraceExperiments
+{
+    class DNSProblem
+    {
+        public void NonResolvableDNS()
+        {
+
+            using (var session = new TraceEventSession("DNS"))
+            {
+
+                var dns = new MicrosoftWindowsDNSClientTraceEventParser(session.Source);
+                session.EnableProvider(MicrosoftWindowsDNSClientTraceEventParser.ProviderGuid);
+                //                session.EnableKernelProvider(KernelTraceEventParser.Keywords.Process);
+
+                var processName = string.Empty;
+                dns.All += (TraceEvent data) => Console.WriteLine(data.ToXml(new StringBuilder()));
+                dns.task_03006 += (task_03006Args data) =>
+                {
+                    try
+                    {
+                        processName = data.ProcessName == string.Empty ? Process.GetProcessById(data.ProcessID).ProcessName : data.ProcessName;
+
+                        Console.WriteLine(processName);
+                        //if (processName == "ClientApp.vshost")
+                        Console.WriteLine(data.Dump());
+                        //Console.WriteLine(data.ToXml(new StringBuilder()));
+                    }
+                    catch (ArgumentException e)
+                    {
+                        //process is dead
+                    }
+                };
+
+
+                // Set up Ctrl-C to stop the session
+                Console.CancelKeyPress += (object s, ConsoleCancelEventArgs args) => session.Stop();
+
+                // Subscribe to a callback that prints the information we wish 
+              
+                // Turn on the process events (includes starts and stops).  
+
+                session.Source.Process();   // Listen (forever) for events
+            }
+
+        }
+
+    }
+}
---
+++ EventTraceExperiments/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTraceExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var session = new TraceEventSession("MyRealTimeSession"))         // Create a session to listen for events
+            {
+                session.Source.Dynamic.All += delegate (TraceEvent data)              // Set Source (stream of events) from session.  
+                {                                                                    // Get dynamic parser (knows about EventSources) 
+                                                                                     // Subscribe to all EventSource events
+                    Console.WriteLine("GOT Event " + data);                          // Print each message as it comes in 
+                };
+
+                var eventSourceGuid = TraceEventProviders.GetEventSourceGuidFromName("MyEventSource"); // Get the unique ID for the eventSouce. 
+                session.EnableProvider(eventSourceGuid);                                               // Enable MyEventSource.
+                session.Source.Process();                                                              // Wait for incoming events (forever).  
+            }
+        }
+    }
+}
