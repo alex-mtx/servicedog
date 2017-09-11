0ae6901557e368899083029c3dc2010e24fc60ac
Alex-MTX <alexander.molinari@gmail.com>
2017-09-11 23:23:22 +0100

    DNS Test passing!

--- Servicedog.Tests/Watchers/ProcessTest.cs
+++ Servicedog.Tests/Watchers/ProcessTest.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 using Servicedog.Watchers;
 using Moq;
 using Servicedog.Messaging;
+using System.Threading;
 
 namespace Servicedog.Tests.Watchers
 {
@@ -14,18 +15,28 @@ namespace Servicedog.Tests.Watchers
     public class ProcessTest
     {
         [Test(TestOf = typeof(DNS))]
-        public void When_A_Dns_Entry_Is_Missing_Should_Notify_Via_Dispatcher()
+        public void When_A_Dns_Lookup_Times_Out_Should_Notify_Via_Dispatcher()
         {
-            var proc = System.Diagnostics.Process.Start("ping", DateTime.Now.Ticks.ToString() + ".unit.test");
+            string falseDnsEntry = DateTime.Now.Ticks.ToString() + ".unit.test";
+            var cancel = new System.Threading.CancellationToken();
 
             var dispatcherMoq = new Mock<IDispatcher>();
-            dispatcherMoq.Setup(x => x.Send(proc.Id,"", DNS.DNS_TIMED_OUT)).Verifiable();
+            dispatcherMoq.Setup(x => x.Send(It.IsAny<int>(), It.IsAny<string>(), DNS.DNS_TIMED_OUT)).Verifiable();
+
             var dns = new DNS(dispatcherMoq.Object);
 
+            //act
+            dns.StartWatching(cancel);
 
+            //now we force a DNS lookup to a non-existing DNS entry
+            var proc = System.Diagnostics.Process.Start("ping", falseDnsEntry);
+            proc.WaitForExit();
+
+            //give some room to ETW to raise the DNS error Event
+            Thread.Sleep(2000);
+
+            //did DNS send a message?
             Assert.DoesNotThrow(() => dispatcherMoq.Verify());
-
-
         }
     }
 }
